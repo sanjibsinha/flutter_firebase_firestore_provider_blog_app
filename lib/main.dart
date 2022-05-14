b30b9484dfbba06aec925e7550dae625e76a3999
Sanjib Sinha <sanjib12sinha@gmail.com>
2022-05-14 10:11:14 +0530

    First of fury

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'view/blog_web_app.dart';
 
+/// this is first branch where we have designed the blog page
+///
+
 void main() {
   runApp(const BlogWebApp());
 }
